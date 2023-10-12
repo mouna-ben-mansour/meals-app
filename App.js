9f8ce685f7376600f21373838a904a3859e4f7a9
mouna-ben-mansour <benmansour.mouna2@gmail.com>
2023-10-12 18:56:54 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import CategoriesScreen from "./screens/CategoriesScreen";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+        <StatusBar style="light" />
+        <CategoriesScreen></CategoriesScreen>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+
+  },
+});
---
+++ components/CategoryGridTile.js
@@ -0,0 +1,40 @@
+import {Pressable, Text, View, StyleSheet, Platform} from "react-native";
+
+function CategoryGridTile({title, color}) {
+    return (
+        <View style={styles.gridItem}>
+            <Pressable android_ripple={{color: '#ccc'}}
+                       style={({ pressed }) => [styles.button, pressed ? styles.buttonPressed : null]}>
+                <View style={[styles.innerContainer ,{backgroundColor: color}]}>
+                    <Text>{title}</Text>
+                </View>
+            </Pressable>
+        </View>
+    )
+}
+export default CategoryGridTile;
+
+const styles = StyleSheet.create( {
+    gridItem: {
+        flex: 1,
+        margin: 16,
+        height: 150,
+        borderRadius: 8,
+        elevation:4,
+        backgroundColor:'white',
+        overflow: Platform.OS === 'android' ? 'hidden' : 'visible'
+    },
+    button: {
+        flex:1
+    },
+    buttonPressed: {
+        opacity: 0.5
+    },
+    innerContainer: {
+        flex:1,
+        padding: 16,
+        borderRadius: 8,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
